8bcb184878a94a88a530791280dfc89d6cf297bf
Afdrian Juarlin <afdrian23@gmail.com>
2021-12-25 16:38:18 +0700

    reusable custom text widget

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit_trip_tips_app/constants/colors.dart';
+import 'package:flutter_cubit_trip_tips_app/widgets/app_large_text.dart';
+import 'package:flutter_cubit_trip_tips_app/widgets/app_text.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_,index){
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/"+images[index]),
+                  fit: BoxFit.cover
+                )
+              ),
+              child: Container(
+                margin: const EdgeInsets.only(top:150,left :20, right:20),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: "Trips"),
+                        AppText(text: "Mountain",size: 30,),
+                        SizedBox(height: 20,),
+                        Container(
+                          width: 250,
+                          child: AppText(text: "Mountain hikes give you an incridible sense of freedom along with endurance test",
+                            color : AppColors.textColor2,
+                            size: 14,
+                          ),
+                        )
+
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }
+          ),
+    );
+  }
+}
