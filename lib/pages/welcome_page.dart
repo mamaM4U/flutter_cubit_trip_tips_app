74bcc83c4209a77ac271f7d594b2538f5d1e404b
Afdrian Juarlin <afdrian23@gmail.com>
2021-12-27 08:32:51 +0700

    index dots, welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cubit_trip_tips_app/constants/colors.dart';
 import 'package:flutter_cubit_trip_tips_app/widgets/app_large_text.dart';
 import 'package:flutter_cubit_trip_tips_app/widgets/app_text.dart';
+import 'package:flutter_cubit_trip_tips_app/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -38,22 +39,38 @@ class _WelcomePageState extends State<WelcomePage> {
               child: Container(
                 margin: const EdgeInsets.only(top:150,left :20, right:20),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         AppLargeText(text: "Trips"),
                         AppText(text: "Mountain",size: 30,),
-                        SizedBox(height: 20,),
+                        const SizedBox(height: 20,),
                         Container(
                           width: 250,
                           child: AppText(text: "Mountain hikes give you an incridible sense of freedom along with endurance test",
                             color : AppColors.textColor2,
                             size: 14,
                           ),
-                        )
+                        ),
+                        const SizedBox(height: 40,),
+                        ResponsiveButton(width: 120,),
 
                       ],
+                    ),
+                    Column(
+                      children: List.generate(images.length, (indexDots){
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index==indexDots?25:8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index==indexDots?AppColors.mainColor:AppColors.mainColor.withOpacity(0.3)
+                          ),
+                        );
+                      }),
                     )
                   ],
                 ),
